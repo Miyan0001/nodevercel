44a45484278075e1e7c9404462a705b902b0ea9d
Miyan0001 <mochamedandryan@gmail.com>
2024-10-28 05:16:52 +0700

    Update

--- api/index.js
+++ api/index.js
@@ -6,29 +6,27 @@ const app = express();
 app.use(express.json());
 
 let data = JSON.stringify({
-  "workspaceId": "miyan0001-miyan-ffstpn29uf7"
+  "workspaceId": "miyan0001-miyan-sq21neorq7f"
 });
 
 let config = {
   method: 'POST',
   url: 'https://gitpod.io/public-api/gitpod.v1.WorkspaceService/StartWorkspace',
   headers: {
-    'User-Agent': 'Mozilla/5.0 (Linux; Android 13; Pixel 7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36',
-    'Accept-Encoding': 'gzip, deflate, br, zstd',
+    'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36',
     'Content-Type': 'application/json',
-    'sec-ch-ua-platform': '"Android"',
-    'sec-ch-ua': '"Android WebView";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
-    'sec-ch-ua-mobile': '?1',
+    'sec-ch-ua': '"Not-A.Brand";v="99", "Chromium";v="124"',
     'connect-protocol-version': '1',
+    'dnt': '1',
+    'sec-ch-ua-mobile': '?1',
+    'sec-ch-ua-platform': '"Android"',
     'origin': 'https://gitpod.io',
-    'x-requested-with': 'mark.via.gp',
     'sec-fetch-site': 'same-origin',
     'sec-fetch-mode': 'cors',
     'sec-fetch-dest': 'empty',
     'referer': 'https://gitpod.io/start/',
-    'accept-language': 'en,en-US;q=0.9',
-    'priority': 'u=1, i',
-    'Cookie': 'GCLB=COa_57b-6Y6N_QEQAw; gitpod_hashed_user_id=172092a222c5d6679cc05bc4f22ec36b; gp-necessary=true; gp-analytical=true; gitpod-marketing-website-visited=true; gp-targeting=true; ajs_anonymous_id=a7c78c3a-28e1-47fd-b422-7381ae34008f; __Host-_gitpod_io_jwt2_=eyJhbGciOiJSUzUxMiIsInR5cCI6IkpXVCIsImtpZCI6IjAwMDEifQ.eyJpYXQiOjE3MzAwMDU3MjYsImV4cCI6MTczMDYxMDUyNiwiaXNzIjoiaHR0cHM6Ly9naXRwb2QuaW8iLCJzdWIiOiI5YWIzMTNjOC02MThhLTQ2NWMtYTJkZS1lYjYzYzU5NTI4NDEifQ.xA5FuL2uVPDKnopSx1HTJCgLrdWVfV3QILvdYLIFA7k70xgtaLNHR3niFyzQqkJV7xVZFJYE5t83WvQ-8MqqGrLdVjyFXIbmldwxcUnyO3nXXeo1n2PSHFPV-R83ecKEnUQIF7JZbQiKday0vIydFVMQX_9hBkt0dnZ7_wZAid3DDgWdA_1vosQMYSKyStPZCufweG_87ilGNqcw5-7OKxYUc0UZQZkQdisREBMaKP7D8wVQ6efD-R59DhZP1evPah060pRmu1qqTtWK9WqqWifhim5PxbpdtDcJ9kmXusZygt1UahPs7Ci61pof5A3NUdNii75t50PxWNnz7qsnky0DtWxYztBvWzvaomHDZ6Ut_gLKpyJVYzHkxCBdKjKMtns0sNMhzjj4eHJOBr3dnoD5qdIfgyIiqA2H66of-rP1MIqqQUMq98Z8ddwXZa8TdxvXoj5R7mLoRAPMZFab9RD_LmHWoxrVU3hIqm-qE6XxN836YPnzEcALwrRlCDyUbJo0W2UUHRtm0ExKMQ3ODpTSTm9uxSHaUF0iRvC7Q3o_lxYpOEYvyFHtRJK46zjdVYj8xnLSj3K6_s-YEhTqf4cpheE8coOoymTrAAYihTwKSLAjatZNIdvXzZKSu1aIb3g8uUp3hpEDK-VZI30itT3TKuzhplh4A4FKxMgT_Y0; _gitpod_io_ws_3b63e904-ada4-4d73-abe4-a55cf08a1f82_owner_=f_v2_GLJ7etCdMeSe82dWnuuC4xg3Djq'
+    'accept-language': 'en-US,en;q=0.9',
+    'Cookie': 'gitpod_hashed_user_id=172092a222c5d6679cc05bc4f22ec36b; gitpod-user=true; gp-necessary=true; gp-analytical=true; gitpod-marketing-website-visited=true; gp-targeting=true; _gcl_au=1.1.323557889.1729804175; ajs_anonymous_id=65012681-ba40-4a88-b736-e1c078a8f563; _ga=GA1.1.1276349055.1729804177; _ga_VWLSB1WXM5=GS1.1.1729804177.1.0.1729804184.0.0.0; _gitpod_io_ws_8846adb4-f951-42d0-8a13-957e712640dd_owner_=SEyJfxyZGE5Gx0UMepaGbqrIcNBoLDus; _gitpod_io_ws_3d787f9a-62b7-4541-bbb9-955f72786c54_owner_=eErdWkWTKxC8Lg778YEjg0o_uJL4aLF9; GCLB=CLenqoKuk9SOFxAD; __Host-_gitpod_io_jwt2_=eyJhbGciOiJSUzUxMiIsInR5cCI6IkpXVCIsImtpZCI6IjAwMDEifQ.eyJpYXQiOjE3MzAwNjcyNDIsImV4cCI6MTczMDY3MjA0MiwiaXNzIjoiaHR0cHM6Ly9naXRwb2QuaW8iLCJzdWIiOiI5YWIzMTNjOC02MThhLTQ2NWMtYTJkZS1lYjYzYzU5NTI4NDEifQ.km5P6J484BVY3UYcozDU6WxYBAxJ6PcvaWz7793osyam3eiXSJh2LTRdA4K0ZGnTVNZ9_67yBJwTTBezMcyLR_PCB-8-qh_MgarZXTQUCMfmwWQf6oZSoP7qS7OSqrmUUEyTYZNM8mbGYC__i0BXAVHJ8pbaqKcKO2FbYmLPSctSkqK7w9TCje0JJUCOrZuHMRBteAQUemmxEECBpEN7a67OnSfAahbn3ALygXFQz_kx_KWmZfHel9VaACHb6cvqIkXjLWMthJZfjZFNVBDA3Y3KLyrrcodaqmj1OhOkDFKmuRHTjAlJnkNnsvXFvrsfqpXtjU_Xuv8YZ5mvD8a-cIDRX6NhgwykRJbiC-34V8y2mtqAb3iglwzOK5dvdgJt2Ip27QmZDIkrAyxUH9sk4XERF5xjKapsMU-TrDzDt8ljA91RuUnhoqx43IazvOsblIQlib8qa1YqU0W1AYMcM2GerNKGAFn861heTKWpM-SabuIyfW5UdJbZM5wWQfXPJT0q848hED-zZR5ReTz7QZ8A63oMV3kSEgaQyLuSPsYQQ1j_NmdjBd0r_jRM1PzSAJs8Pw3Q6fG32hwSlh8AqrJ-AGC_6z8JIXaiIjKE2X5OzWPYGBRrlxKqF4GSHYBDpCZCYQhaViGR8OcHdJGviT-zigKEAAoAjFyx_Ma_l1A'
   },
   data: data
 };
